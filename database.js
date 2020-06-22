b62703f6c6aef9bc4603f5a95cb3a61b9bf83781
unknown <metgudmath.ashwini7@gmail.com>
2020-06-22 07:21:00 +0200

    changed db file

--- database.js
+++ database.js
@@ -3,10 +3,12 @@ const mysql = require('mysql');
 require("dotenv").config();
 
 var connection = mysql.createConnection({
+  connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    multipleStatements: true
   });
 
 
